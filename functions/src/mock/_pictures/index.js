2d696e6e26ff40f4cd116397a45b29e10303ce3e
ThibaultSavary <thibaultsavary@protonmail.ch>
2018-01-05 11:43:10 +0100

    REMOVE MOCK FROM MASTER

--- functions/src/mock/_pictures/index.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs')
-const files = fs.readdirSync('./')
-const pictures = files.map(fileName => fs.readFileSync(fileName).toString('base64'))
-fs.writeFileSync('pictures.json', JSON.stringify(pictures))
