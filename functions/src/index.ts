1087f14bd5d6c064c24c2bf9be84efb2c67fc01d
lahode <lahode@infomaniak.ch>
2018-08-01 15:05:34 +0200

    Retirer la fonction d'ajout automatique dans public/users/profiles (qui ne fonctionne pas)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 admin.initializeApp(functions.config().firebase)
-export { accounting, annualBreedsStatistics, annualRevenueStatistics, backup, onCustomerTransfer, onProfileUpdate, onUserCreate, onYearRead } from './fn/index'
+// export { accounting, annualBreedsStatistics, annualRevenueStatistics, backup, onCustomerTransfer, onProfileUpdate, onUserCreate, onYearRead } from './fn/index'
+export { accounting, annualBreedsStatistics, annualRevenueStatistics, backup, onCustomerTransfer, onUserCreate, onYearRead } from './fn/index'
